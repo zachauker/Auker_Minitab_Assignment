c0c8214370d865bfa786a421d492dd0bb971651c
Zachary Auker <zachary.auker@gmail.com>
2025-04-21 15:25:50 -0400

    Creating entities --wip--

--- Auker_Minitab_Interview_Assignment/Entities/Address.cs
+++ Auker_Minitab_Interview_Assignment/Entities/Address.cs
@@ -2,5 +2,9 @@ namespace Auker_Minitab_Interview_Assignment.Entities;
 
 public class Address
 {
-    
+    public string Line1 { get; set; }
+    public string City { get; set; }
+    public string State { get; set; }
+    public string PostalCode { get; set; }
+    public string Country { get; set; }
 }
--- Auker_Minitab_Interview_Assignment/Entities/Customer.cs
+++ Auker_Minitab_Interview_Assignment/Entities/Customer.cs
@@ -2,5 +2,7 @@ namespace Auker_Minitab_Interview_Assignment.Entities;
 
 public class Customer
 {
-    
+    public string Name { get; set; }
+    public string EmailAddress { get; set; }
+    public Address Address { get; set; }
 }
